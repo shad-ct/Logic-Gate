a8635edba5b8814166e262482016e91f549ca0db
S D <88387228+shau-14@users.noreply.github.com>
2024-07-13 13:22:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
 let outputOR = document.getElementById("outputOR")
 let test = document.getElementById('testing')
 
-function submitOR(){
-    
-    let inpOR1 = document.getElementById('inpOR1').value
-    let inpOR2 = document.getElementById('inpOR2').value;
 
 
-    valOR = parseInt(inpOR1) + parseInt(inpOR2);
-    
-    if(valOR == 1){
-        outputOR.innerHTML = "True"
-    }
-    else if(valOR == 0){
-        outputOR.innerHTML = "False"
-    }
-    else{
-        alert("Make sure you input 1 or 0 ")
-    }
-    // test.innerHTML= "val 1 is " + inpOR1 + " val 2 is " + inpOR2 + " their sum is : " + val
-}
+
+
 
 
 
@@ -27,10 +12,22 @@ let outputAND = document.getElementById("outputAND")
 
 function submitAND(){
     
-    let inpAND1 = document.getElementById('inpAND1').value
-    let inpAND2 = document.getElementById('inpAND2').value;
 
+    let inpAND01 = document.getElementById('inpAND1').value
+    let inpAND02 = document.getElementById('inpAND2').value;
+    
+    inpAND1 = parseInt(inpAND01)
+    inpAND2 = parseInt(inpAND02)
 
+    if (!((inpAND1 === 1 || inpAND1 === 0) && (inpAND2 === 1 || inpAND2 === 0))) {
+        outputAND.innerHTML = "Invalid Input";
+    }
+    
+    else{
+        outputAND.innerHTML = inpAND1&&inpAND2
+    }
+
+    /*
     valAND = parseInt(inpAND1) * parseInt(inpAND2);
     
     if(valAND == 1){
@@ -42,6 +39,7 @@ function submitAND(){
     else{
         alert("Make sure you input 1 or 0 ")
     }
+        */
     // test.innerHTML= "val 1 is " + inpOR1 + " val 2 is " + inpOR2 + " their sum is : " + val
 }
 
@@ -52,22 +50,50 @@ function submitAND(){
 
 
 
+function submitOR(){
+    
+    let inpOR01 = document.getElementById('inpOR1').value
+    let inpOR02 = document.getElementById('inpOR2').value;
+
+    inpOR1 = parseInt(inpOR01)
+    inpOR2 = parseInt(inpOR02)
+
+    // valOR = parseInt(inpOR1) + parseInt(inpOR2);
+    if (!((inpOR1 === 1 || inpOR1 === 0) && (inpOR2 === 1 || inpOR2 === 0))) {
+        outputOR.innerHTML = "Invalid Input";
+    }
+    
+    else{
+        outputOR.innerHTML = inpOR1&&inpOR2
+    }
+    /*
+    if(valOR == 1){
+        outputOR.innerHTML = "True"
+    }
+    else if(valOR == 0){
+        outputOR.innerHTML = "False"
+    }
+    else{
+        alert("Make sure you input 1 or 0 ")
+    }*/
+   
+    // test.innerHTML= "val 1 is " + inpOR1 + " val 2 is " + inpOR2 + " their sum is : " + val
+}
+
+
+
+
+
 let outputNOT = document.getElementById("outputNOT")
 
 function submitNOT(){
     
-    let inpNOT1 = document.getElementById('inpNOT1').value
-
-
-    valNOT = parseInt(inpNOT1) ;
-    
-    if(valNOT == 1){
-        outputNOT.innerHTML = "False : 0"
-    }
-    else if(valNOT == 0){
-        outputNOT.innerHTML = "True : 1"
-    }
-    else{
-        alert("Make sure you input 1 or 0 ")
-    }
+    let inpNOT01 = document.getElementById('inpNOT1').value
+    inpNOT1 = parseInt(inpNOT01);
+    if (inpNOT1 === 1 || inpNOT1 === 0) {
+        outputNOT.innerHTML = !inpNOT1
+        }
+        else{
+            outputNOT.innerHTML = "Invalid Input"
+            }
 }
